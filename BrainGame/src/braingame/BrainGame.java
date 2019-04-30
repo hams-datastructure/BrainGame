a718dd9b8a958a0500c03b8599649b6fdc13be1b
YM Tengku Mimi <YM Tengku Mimi@Tengku-Mimi>
2019-04-30 16:23:43 +0800

    testlagi

--- BrainGame/src/braingame/BrainGame.java
+++ BrainGame/src/braingame/BrainGame.java
@@ -17,6 +17,7 @@ public class BrainGame {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("haiii awakk");
+        System.out.println("saya");
     }
     
 }
